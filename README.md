dea72e09ac3dfe85cfa13ca4af89c05fe31a6b78
karina <karinaanton77@gmail.com>
2020-11-08 19:54:32 +0300

    Changed dataset for experiment

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Parallelization of the ellipsoid method for risk minimization
+# Ellipsoid method for convex stochastic optimization in small dimension
 
 ## Introduction
 
-This is the implementation of the experiments, which were done for the article ["Parallelization of the ellipsoid method for risk minimization"](link_hear). 
+This is the implementation of the experiments, which were done for the article ["Ellipsoid method for convex stochastic optimization in small dimension"](link_hear). 
 Our ellipsoid method realisation extends [torch.optimizer.Optimizer](https://pytorch.org/docs/stable/_modules/torch/optim/optimizer.html#Optimizer), consequently it can be used for neural network trainings.
 
 ## Navigation
--- experiment/utils/plot.py
+++ experiment/utils/plot.py
@@ -1,16 +1,20 @@
 import matplotlib.pyplot as plt
 
-def plot_results(losses_ell, losses_sgd, batch_sz, lim):
+def plot_results(losses_sgd_small_batch_size, losses_ell, losses_sgd, batch_sz, batch_sz_sgd, lim = -1):
   if len(losses_ell) != 0:
-    plt.plot(losses_ell, label="Ellipsoid method", color="C0")
+    plt.plot(losses_ell, label="Ellipsoid method, batch size = " + str(batch_sz), color="C0")
 
   if len(losses_sgd) != 0:
-    plt.plot(losses_sgd, label= "SGD", color="C1")
+    plt.plot(losses_sgd, label= "SGD, batch size = " + str(batch_sz), color="C1")
+
+  if len(losses_sgd_small_batch_size) != 0:
+    plt.plot(losses_sgd_small_batch_size, linestyle = '--', label= "SGD, batch size = " + str(batch_sz_sgd), color="C2")
 
   plt.grid()
   plt.legend()
-  plt.ylim(top=lim)
+  if lim != -1:
+    plt.ylim(bottom=0.58, top=lim)
   plt.ylabel("Loss")
   plt.xlabel("Iteration")
-  plt.title("Batch size = " + str(batch_sz))
+  plt.title("Test loss on iterations")
   plt.show()
